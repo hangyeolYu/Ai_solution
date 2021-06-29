c47b3a2718f37f75427a4f1ab1eff68fd285f765
hangyeolYu <hangyeolem@gmail.com>
2021-06-29 10:08:44 +0900

    20210629
    프레임 워크 Map 과 HashSep  공부 내용추가 ,프로젝트 최종 완료

---
+++ src/ai_solution/Ideal.java
@@ -0,0 +1,9 @@
+package ai_solution;
+
+public class Ideal extends standard {
+
+    public Ideal() {}
+    public Ideal(int age, String male, String live, int one_time){
+        super(age, male, live, one_time);
+    }
+}
---
+++ src/ai_solution/Likely.java
@@ -0,0 +1,10 @@
+package ai_solution;
+
+public class Likely extends standard {
+
+    public Likely() {}
+    public Likely(int age, String male, String live, int one_time){
+
+        super(age, male, live, one_time);
+    }
+}
